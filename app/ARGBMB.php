65fe17a516d434cbdce08bc3afb4a8ff16ae0b88
Serkan Ciracioglu <sciracioglu@tedankara.k12.tr>
2018-11-23 17:26:39 +0300

    update tarihi

--- app/ARGBMB.php
+++ app/ARGBMB.php
@@ -7,9 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class ARGBMB extends Model
 {
     protected $table      = 'ARGBMB';
+    public $timestamps    = false;
+
     protected $fillable   = [
         'GONDERILDI',
         'ISLEM',
         'ISLEMTARIH'
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+        static::updating(function ($model) {
+            $model->ISLEMTARIH = date('d/m/Y H:i:s');
+        });
+    }
 }
