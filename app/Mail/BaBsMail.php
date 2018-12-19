c2b7acfdc1be1c18ffd128ccd524552968d06723
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2018-12-19 19:56:47 +0300

    ay gosterimi

--- app/Mail/BaBsMail.php
+++ app/Mail/BaBsMail.php
@@ -35,8 +35,9 @@ class BaBsMail extends Mailable
             'locale'   => 'tr_TR',
             'timezone' => 'Europe/Istanbul',
         ]);
-
-        $this->tarih = $date->year . ' / ' . $date->monthName;
+        $aylar       = [1=>'Ocak', 2=>'Şubat', 3=>'Mart', 4=>'Nisan', 5=>'Mayıs', 6=>'Haziran', 7=>'Temmuz', 8=>'Ağustos', 9=>'Eylül', 10=>'Ekim', 11=>'Kasım', 12=>'Aralık'];
+        $ay          = date('n');
+        $this->tarih = date('Y') . ' / ' . $aylar[$ay];
 
         return $this->to($this->data->EMAIL5)
                     ->subject($this->data->SIRKETAD . ' BS Mütabakat')
