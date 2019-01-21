6801644579dd93525c41725abd066e4d2e4fbc8d
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2019-01-21 22:15:53 +0300

    mail validation

--- app/Http/Controllers/BaBsController.php
+++ app/Http/Controllers/BaBsController.php
@@ -18,8 +18,10 @@ class BaBsController extends Controller
                         ->get();
         if ($data->count() > 0) {
             foreach ($data as $firma) {
-                Mail::to($firma->EMAIL5)
+                if ($firma->EMAIL5 && (filter_var($firma->EMAIL5, FILTER_VALIDATE_EMAIL))) {
+                    Mail::to($firma->EMAIL5)
                         ->send(new BaBsMail($firma));
+                }
                 ARGBYNBS::where('GUID', $firma->GUID)->update([
                     'GONDERILDI' => 1
                 ]);
